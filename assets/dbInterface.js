4d35f94905cc215e9cbecff70688c2db5f08943f
Fernando Mayorga <mayorgarojasgamedeveloper>
2018-05-01 13:54:41 -0500

    primera prueba de coneccion de bd.

--- assets/dbInterface.js
+++ assets/dbInterface.js
@@ -1,15 +1,51 @@
+var {Client} = require('pg');
+
+var URL = process.env.URL;
+var client = new Client(URL);
+
+client.connect((err) => {
+  if(err)
+    console.error(err);
+  else
+    console.log('Database Started');
+});
+
 exports.INSERT = function() {
+  /*
+  var sql = `INSERT INTO ${table} (${attr}) VALUES (${values})`;
+  if(returning !== null)
+    sql += ` RETURNING ${returning}`
+
+  return await client.query(sql);
+  */
   return {status: 'Falta programar DB dbInterface INSERT'};
 }
 
 exports.SELECT = function() {
+  /*
+  var sql = `SELECT ${attr} FROM ${table}`;
+  if(where !== null)
+    sql += ` WHERE ${where}`;
+  if(orderBy !== null)
+    sql += ` ORDER BY ${orderBy}`;
+
+  return await client.query(sql);
+  */
   return {status: 'Falta programar DB dbInterface SELECT'};
 }
 
 exports.UPDATE = function() {
+  /*
+  var sql = `UPDATE ${table} SET ${setters} WHERE ${where}`;
+  return await client.query(sql);
+  */
   return {status: 'Falta programar DB dbInterface UPDATE'};
 }
 
 exports.DELETE = function() {
+  /*
+  var sql = `DELETE FROM ${table} WHERE ${where}`
+  return await client.query(sql);
+  */
   return {status: 'Falta programar DB dbInterface DELETE'};
 }
