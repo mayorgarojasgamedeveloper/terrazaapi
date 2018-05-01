f9445f1da3fe1b6243763cd4000c64486af7a934
Fernando Mayorga <mayorgarojasgamedeveloper>
2018-05-01 13:49:49 -0500

    Se terminaron todas las rutas. Solo fata el control de los datos y la coneccion con la bd.

---
+++ controllers/usuarioController.js
@@ -0,0 +1,26 @@
+var model = require('../models/usuarioModel');
+
+exports.create = function(req, res) {
+  var response = model.create();
+  return response;
+};
+
+exports.list = function(req, res) {
+  var response = model.list();
+  return response;
+};
+
+exports.view = function(req, res) {
+  var response = model.view();
+  return response;
+};
+
+exports.edit = function(req, res) {
+  var response = model.edit();
+  return response;
+};
+
+exports.delete = function(req, res) {
+  var response = model.delete();
+  return response;
+};
--- routes/usuario.js
+++ routes/usuario.js
@@ -1,24 +1,30 @@
 var express = require('express');
+var controller = require('../controllers/usuarioController');
 var router = express.Router();
 
 router.post('/', function(req, res, next) {
-  res.json({status: 'Falta programa esto'});
+  var response = controller.create(req, res);
+  res.json(response);
 });
 
 router.get('/', function(req, res, next) {
-  res.json({status: 'Falta programa esto'});
+  var response = controller.list(req, res);
+  res.json(response);
 });
 
 router.get('/:id', function(req, res, next) {
-  res.json({status: 'Falta programa esto'});
+  var response = controller.view(req, res);
+  res.json(response);
 });
 
 router.put('/', function(req, res, next) {
-  res.json({status: 'Falta programa esto'});
+  var response = controller.edit(req, res);
+  res.json(response);
 });
 
 router.delete('/', function(req, res, next) {
-  res.json({status: 'Falta programa esto'});
+  var response = controller.delete(req, res);
+  res.json(response);
 });
 
 module.exports = router;
