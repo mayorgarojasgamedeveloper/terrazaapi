443933d3937a492fb870d2d4acd4eb24ebc056e0
Fernando Mayorga <mayorgarojasgamedeveloper>
2018-05-13 10:23:39 -0500

    Cabezeras otra vez

--- app.js
+++ app.js
@@ -20,11 +20,21 @@ app.set('view engine', 'jade');
 
 // Cors
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'post,get,put,delete');
-    res.header("Access-Control-Allow-Headers", '*');
-    next();
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', false);
+
+  // Pass to next layer of middleware
+  next();
 });
 
 
