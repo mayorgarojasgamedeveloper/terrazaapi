bbe8ac639eb2bd57addca3d09b6e0bd33d2f4153
Fernando Mayorga <mayorgarojasgamedeveloper>
2018-05-12 23:11:35 -0500

    Origin

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set('view engine', 'jade');
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Methods', '*');
     res.header("Access-Control-Allow-Headers", '*');
     next();
 });
