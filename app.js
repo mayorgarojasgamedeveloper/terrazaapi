33625f8e3565d77b84613641266234117b4fa7d8
Fernando Mayorga <mayorgarojasgamedeveloper>
2018-05-15 12:43:44 -0500

    Se manejan premises ahora

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var clientRouter = require('./routes/cliente');
@@ -25,13 +26,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // CORS
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
-});
-
+});*/
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/client', clientRouter);
 app.use('/event', eventRouter);
